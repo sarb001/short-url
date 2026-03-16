3a40a80a835e1231ad182f5cfd719d8e98bb4c3e
iamsarb001 <sarbbsandhu555@gmail.com>
2026-03-16 08:00:31 +0530

    headers -working

--- middleware/auth.js
+++ middleware/auth.js
@@ -18,17 +18,37 @@ import { getUser } from '../service/serviceauth.js';
 // }
 
 
+// async function authmiddleware(req,res,next){
+ 
+//     const usertoken = req.cookies?.token;                          
+//     console.log('user jwt token -',usertoken);
+//     if(!usertoken)   return res.redirect('/login');      // no cookies found
+
+//     console.log('after login =-')
+//     const user = getUser(usertoken);                    // checking user
+//     if(!user) return  res.redirect('/login');        // no user found 
+
+//     console.log('user assgn');
+//     req.user = user;
+//     next();
+// }
+
+// Do it with headers => 
+
 async function authmiddleware(req,res,next){
  
-    const usertoken = req.cookies?.token;                          
-    console.log('user jwt token -',usertoken);
+    console.log(' request heade s- -',req.headers);
+    const usertoken = req.headers['authorization'];
+
+    const Newtoken = usertoken.split("Bearer ")[1];
+    console.log('user New token -',Newtoken);
     if(!usertoken)   return res.redirect('/login');      // no cookies found
 
     console.log('after login =-')
-    const user = getUser(usertoken);                    // checking user
+    const user = getUser(Newtoken);                    // checking user
     if(!user) return  res.redirect('/login');        // no user found 
 
-    console.log('user assgn');
+    console.log('user asigned-',user);
     req.user = user;
     next();
 }
